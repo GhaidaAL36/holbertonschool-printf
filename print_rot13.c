99b6a1eecafe33b1897c93794c1fb95216e4e06a
GhaidaAl36 <ghaida3638@gmail.com>
2025-10-02 06:44:26 +0000

    fix betty style for print_rot13.c

--- print_rot13.c
+++ print_rot13.c
@@ -19,7 +19,9 @@ int print_rot13(const char *s)
 		if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
 		{
 			char base = (c >= 'a') ? 'a' : 'A';
+
 			c = (char)(base + ((c - base + 13) % 26));
+
 		}
 		if (_putchar(c) < 0)
 			return (-1);
